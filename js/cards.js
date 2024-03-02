dc3631befec796f2c1b39630101a358d6b0f0e69
Re1gen <djumamaulen@gmail.com>
2024-03-02 14:57:15 +0600

    DZ-7

--- js/cards.js
+++ js/cards.js
@@ -1,28 +1,28 @@
-// const getCards = async () =>{
-//     try{
-//     const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=12")
-//     const data = await response.json()
-//     console.log(data)
-//         const displayCards = (data) =>{
-//             const cardContainer = document.querySelector(".cardContainer")
-//             cardContainer.innerHTML = ""
-//
-//             data.forEach(post => {
-//                 const card = document.createElement("div")
-//                 card.classList.add("card")
-//                 card.innerHTML =`
-//                     <div class="card_img">
-//                     </div>
-//                     <h3>${post.title}</h3>
-//                     <p>${post.body}</p>
-//                     `
-//                 cardContainer.appendChild(card);
-//             })
-//         }
-//         displayCards(data)
-//     } catch (error){
-//         console.log(error)
-//     }
-// }
-//
-// getCards()
+const getCards = async () =>{
+    try{
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=12")
+    const data = await response.json()
+    console.log(data)
+        const displayCards = (data) =>{
+            const cardContainer = document.querySelector(".cardContainer")
+            cardContainer.innerHTML = ""
+
+            data.forEach(post => {
+                const card = document.createElement("div")
+                card.classList.add("card")
+                card.innerHTML =`
+                    <div class="card_img">
+                    </div>
+                    <h3>${post.title}</h3>
+                    <p>${post.body}</p>
+                    `
+                cardContainer.appendChild(card);
+            })
+        }
+        displayCards(data)
+    } catch (error){
+        console.log(error)
+    }
+}
+
+getCards()
